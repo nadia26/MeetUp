43f8cdb190577b313d5ab890c767ae10bf41dc39
Aida Piccato <aidapiccato@gmail.com>
2015-05-05 19:24:04 -0400

    Added current location option

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,22 @@
 
 var locateButtons = document.getElementsByName("locate");
-var a1 = locateButtons[0];
-a1.addEventListener('click',function(e) {console.log('click')});
+var b1 = locateButtons[0];
+var setCurrentLocation = function(position) {
+	var longitude = document.getElementById("longitude");
+	var latitude = document.getElementById("latitude");
+	longitude.innerHTML = position.coords.longitude;
+	latitude.innerHTML = position.coords.latitude;
+	var a1 = document.getElementById("address1");
+	a1.disabled = true
+	a1.placeholder = "Using your current coordinates...";
+}
+var getCurrentLocation = function(){
+	if( navigator.geolocation ) {
+		navigator.geolocation.getCurrentPosition(setCurrentLocation);
+	}
+}
+
+b1.addEventListener('click',getCurrentLocation);
 function initialize() {
     var mapOptions = {
 	center: { lat:40.7881,  lng: -73.95,},
