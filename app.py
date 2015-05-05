9eb19453d3d0538e7a2488bcaef41232fc646944
Aida Piccato <aidapiccato@gmail.com>
2015-05-04 21:51:04 -0400

    modal and form now works

--- app.py
+++ app.py
@@ -2,10 +2,21 @@ from flask import Flask,request,url_for,redirect,render_template
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
-    return render_template("main.html")
+	if request.method == "POST":
+		print request.form
+		addresses = {
+			'address1': request.form['address1'],
+			'address2': request.form['address2']
+		}
+		return redirect(url_for('results', addresses=addresses))
+   	else:
+   		return render_template("main.html")
 
+@app.route("/results/<addresses>", methods=["GET","POST"])
+def results(addresses):
+	return render_template("results.html")
 
 if __name__=="__main__":
    app.debug=True
