52361e0e9456af808858d8fb4215253613dbbb0e
apiccato <aidapiccato@gmail.com>
2015-05-04 20:25:37 -0400

    Added maps and main.html

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask,request,url_for,redirect,render_template
-import utils
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    return render_template("main.html")
 
 
 if __name__=="__main__":
--- static/js/maps.js
+++
@@ -1,9 +0,0 @@
-function initialize() {
-    var mapOptions = {
-        center: { lat: 40.815, lng: -74.0059},
-        zoom: 12
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-				  mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
