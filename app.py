fef2b2b98a9d783df9e097f25e19a143445f51cd
Nadia Saleh <nadiasaleh26@gmail.com>
2015-05-06 12:33:16 -0400

    splitting string

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
-from flask import Flask, request, url_for, redirect, render_template
+from flask import Flask,request,url_for,redirect,render_template
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-
-
-@app.route("/", methods=["GET", "POST"] )
+@app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        print "post"
-        addresses = [ request.form['address1'], request.form['address2'] ]
-        print addresses
+        print request.form
+        addresses = [
+            request.form['address1'],
+            request.form['address2']
+        ]
         return redirect(url_for('results', addresses=addresses))
     else:
-        print "get"
         return render_template("main.html")
 
-
-
-@app.route("/results/<addresses>", methods=["GET", "POST"] )
+@app.route("/results/<addresses>", methods=["GET","POST"])
 def results(addresses):
-    print "results"
-    print addresses
+    addresses = "[u'AAAAAA', u'BBBBB']"
+addresses = addresses[1:-1]
+addresses = addresses.split(', ')
+for i in range (len (addresses) ):
+    x = addresses[i]
+    addresses[i] = x[2:-1]
+print addresses
     return render_template("results.html", addresses = addresses)
 
-
-if __name__ == "__main__":
-    app.debug = True
+if __name__=="__main__":
+    app.debug=True
     app.run()
