c2199902d8cf54591273a4a0b49810c1b38f29f6
Nadia Saleh <nadiasaleh26@gmail.com>
2015-05-06 11:00:25 -0400

    ugh

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
-from flask import Flask,request,url_for,redirect,render_template
+from flask import Flask, request, url_for, redirect, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+
+
+@app.route("/", methods=["GET", "POST"] )
 def index():
-	if request.method == "POST":
-		print request.form
-		addresses = {
-			'address1': request.form['address1'],
-			'address2': request.form['address2']
-		}
-		return redirect(url_for('results',addresses=addresses))
-   	else:
-   		return render_template("main.html")
-
-@app.route("/results/<addresses>", methods=["GET","POST"])
-def results(addresses):
-        print "ADDRESSES:"
+    if request.method == "POST":
+        print "post"
+        addresses = [ request.form['address1'], request.form['address2'] ]
         print addresses
-	return render_template("results.html", addresses=addresses)
+        return redirect(url_for('results', addresses=addresses))
+    else:
+        print "get"
+        return render_template("main.html")
 
-if __name__=="__main__":
-   app.debug=True
-   app.run()
+
+
+@app.route("/results/<addresses>", methods=["GET", "POST"] )
+def results(addresses):
+    print "results"
+    print addresses
+    return render_template("results.html", addresses = addresses)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
