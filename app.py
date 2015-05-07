965a752d09c194f4eb3da53475e272851dca3e21
Nadia Saleh <nadiasaleh26@gmail.com>
2015-05-07 12:31:50 -0400

    results js

--- app.py
+++ app.py
@@ -6,23 +6,16 @@ app=Flask(__name__)
 def index():
     if request.method == "POST":
         print request.form
-        addresses = [
-            request.form['address1'],
-            request.form['address2']
-        ]
+        addresses = {
+            'address1':request.form['address1'],
+            'address2':request.form['address2']
+        }
         return redirect(url_for('results', addresses=addresses))
     else:
         return render_template("main.html")
 
 @app.route("/results/<addresses>", methods=["GET","POST"])
 def results(addresses):
-    addresses = "[u'AAAAAA', u'BBBBB']"
-addresses = addresses[1:-1]
-addresses = addresses.split(', ')
-for i in range (len (addresses) ):
-    x = addresses[i]
-    addresses[i] = x[2:-1]
-print addresses
     return render_template("results.html", addresses = addresses)
 
 if __name__=="__main__":
