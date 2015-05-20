918524e8886d0caff5cd126b1c06be3ac98c43e6
Aida Piccato <aidapiccato@gmail.com>
2015-05-20 13:24:15 -0400

    linked panels to markers on map

--- static/js/places.js
+++ static/js/places.js
@@ -5,7 +5,6 @@ App.addRegions({
 var RestaurantCollection = Backbone.Collection.extend({
   model: RestaurantModel
 });
-
 var RestaurantModel = Backbone.Model.extend({
   url: "/restaurant",
   idAttribute: "_id"
@@ -13,9 +12,15 @@ var RestaurantModel = Backbone.Model.extend({
 
 App.RestaurantView = Marionette.ItemView.extend({
   template: "#restaurant-template",
-  tagName: "panel panel-default"
+  tagName: "panel panel-default",
+  events: {
+    "click .panel-title": function() {
+      console.log(this);
+      infowindow.setContent(this.model.attributes.name);
+      infowindow.open(map,this.model.attributes.marker);
+    }
+  }
 });
-
 App.RestaurantsCompositeView = Marionette.CompositeView.extend({
   childView: App.RestaurantView,
   childViewContainer : "#accordion",
@@ -23,32 +28,37 @@ App.RestaurantsCompositeView = Marionette.CompositeView.extend({
 });
 
 var restaurants = new RestaurantCollection();
-var initialized = false;
-
+var bakeries = new RestaurantCollection();
+var initialized = {
+  "restaurant" : false,
+  "bakery" : false
+}
 var infowindow;
 
-function initializePlaces() {
-	var request = {
-		location: a,
-		radius: 500,
-		types: ["restaurant"]
-	};
-	infowindow = new google.maps.InfoWindow();
-	var service = new google.maps.places.PlacesService(map);
-	service.nearbySearch(request, callback);
+function initialize(type) {
+  var request = {
+    location: a,
+    radius: 500,
+    types: [type]
+  };
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 }
 
 function callback(results, status) {
   console.log(status);
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
-      console.log(results[i]);
       results[i]["open_string"] = "";
-      if (results[i]["opening_hours"]["open_now"]) {
+      if ( results[i]["opening_hours"] && results[i]["opening_hours"]["open_now"]) {
         results[i]["open_string"] = "Open Now";
       }
       else {
         results[i]["open_string"] = "Not Open"
+      }
+      if (!results[i]["rating"]) {
+        results[i]["rating"] = 0;
       }
       var p = parseInt(results[i]["price_level"]);
       results[i]["price_string"] = "";
@@ -56,31 +66,50 @@ function callback(results, status) {
         results[i]["price_string"] += "$";
         p = p - 1;
       };
-      restaurants.unshift(new RestaurantModel(results[i]));
-  		createMarker(results[i]);
+      results[i]["marker"] = createMarker(results[i]);
+      if (results[0]["types"].indexOf("bakery") > -1) {
+        bakeries.unshift(new RestaurantModel(results[i]));
+      }
+      else {
+        restaurants.unshift(new RestaurantModel(results[i]));
+      }
+  		
      }
 		}
     console.log(restaurants);
 }
+
 function createMarker(place) {
   var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
     map: map,
-    position: place.geometry.location
+    position: place.geometry.location,
   });
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent(place.name);
     infowindow.open(map, this);
+
   });
+  return marker;
 }
 
 
-document.getElementById("display").addEventListener('click', function(e) {
-  if (!initialized){
-    initializePlaces();
-    initialized = true;
+document.getElementById("displayRestaurants").addEventListener('click', function(e) {
+  if (!initialized["restaurant"]){
+    initialize("restaurant");
+    initialized["restaurant"] = true;
   };
   var rcompview = new App.RestaurantsCompositeView({collection:restaurants});
   App.collregion.show(rcompview);
-	document.getElementById("display").setAttribute("hidden", "hidden");
+	//document.getElementById("display").setAttribute("hidden", "hidden");
+});
+
+document.getElementById("displayBakeries").addEventListener('click', function(e) {
+  if (!initialized["bakery"]){
+    initialize("bakery");
+    initialized["bakery"] = true;
+  };
+  var bcompview = new App.RestaurantsCompositeView({collection:bakeries});
+  App.collregion.show(bcompview);
+  //document.getElementById("display").setAttribute("hidden", "hidden");
 });
