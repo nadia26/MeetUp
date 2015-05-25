fadbaad0038fb1e39c955feb7f5962ef11bfb11f
sydneyacaputo <sydneyacaputo@gmail.com>
2015-05-25 16:39:59 -0400

    got info into an array

--- static/js/events.js
+++ static/js/events.js
@@ -1,13 +1,17 @@
 
-
-
+var midPoint= "40.7178801,-74.0137509";
+var eventsArray=[];
 
 var getEvents= $.ajax({
-	url: "http://api.eventful.com/json/events/search?&app_key=cWNxSHrggxxJH23h&where=32.746682,-117.162741&within=1&date=Today",
+	url: "http://api.eventful.com/json/events/search?&app_key=cWNxSHrggxxJH23h&where="+midPoint+"&within=1&date=Today",
 	dataType: 'jsonp',
 	success: function(results){
-		console.log(results);
+		for (i=0; i < results["events"]["event"].length; i++){
+			eventsArray.push(results["events"]["event"][""+i+""]);
+		}
+		console.log(eventsArray[0]);
+		console.log(eventsArray);
 	}
 });
 
-$("#loadRes").click(getEvents);	
+$("loadRes").click(getEvents);	
