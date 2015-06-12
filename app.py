52a0423a4105727bce96efe4cb078bca1fd46bca
Nadia <nadiasaleh26@gmail.com>
2015-06-12 10:27:02 -0400

    more deletions

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app=Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        #print request.form
         addresses = {
             'address1':request.form['address1'],
             'address2':request.form['address2']
@@ -16,19 +15,9 @@ def index():
 
 
 
-@app.route("/auto", methods=["GET", "POST"])
-def auto():
-    return render_template("autotest.html")
-
-
 @app.route("/directions/<addresses>", methods=["GET", "POST"])
 def directions(addresses):
     return render_template("directions.html", addresses=addresses)
-
-
-@app.route("/test/<addresses>", methods=["GET", "POST"])
-def test(addresses):
-    return render_template("test2.html", addresses=addresses)
 
 
 if __name__=="__main__":
